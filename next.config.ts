5117add77da26886891428f46c7d67f1e3162d2c
ai-prgmr <3.14.ai.prgmr@gmail.com>
2026-07-08 16:56:30 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/grow-more",
   output: "export",
   images: {
     minimumCacheTTL: 31536000,
